adefa9f1b6bbaa401b1e1c147a8f092c0ffff274
Anthony Kim <akim2214@gmail.com>
2018-03-04 22:31:36 -0800

    added mock data

---
+++ app/data/friends.js
@@ -0,0 +1,66 @@
+var friends = [
+
+    {
+        name: "Snoopy",
+        photo: "https://upload.wikimedia.org/wikipedia/en/5/53/Snoopy_Peanuts.png",
+        scores: [
+        2,
+        1,
+        1,
+        4,
+        5,
+        4,
+        3,
+        2,
+        1,
+        5,
+        ]
+    }, {	
+        name: "Hobbes",
+        photo: "https://i.pinimg.com/564x/a1/76/69/a17669d9c46b548ba79708afe7ecded4--group-work.jpg",
+        scores: [
+        1, 
+        1, 
+        1, 
+        1, 
+        1, 
+        1, 
+        1, 
+        1, 
+        1, 
+        1,
+        ]
+    }, {
+        name: "Tom",
+        photo: "http://www.todayifoundout.com/wp-content/uploads/2017/12/myspace-tom.jpg",
+        scores: [
+        2,
+        4,
+        1,
+        5,
+        5,
+        3,
+        2,
+        1,
+        3,
+        4,
+        ]
+    }, {
+        name: "Sigmund",
+        photo: "http://www.nndb.com/people/736/000029649/sigmund-freud-med.jpg",
+        scores: [
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        5,
+        ]
+    },    
+    ];
+   
+    module.exports = friends;
